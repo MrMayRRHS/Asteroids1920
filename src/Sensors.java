36f5c6fcbf47d7a0ed3cc019f3cf2972b51e80c8
EAusman <emma.ausman@gmail.com>
2019-12-03 11:44:31 -0600

    fixing merge errors

--- src/Sensors.java
+++ src/Sensors.java
@@ -3,28 +3,29 @@ import java.util.*;
 
 public class Sensors extends GameView
 { 
-  private ArrayList<NetworkActor> asteroids;
-   
-  //800 by 600 screen per person
-  public Sensors(Client client, GameState state){
-    super(client,state);
-    asteroids = getAsteroids(); 
-  }
-  public ArrayList<NetworkActor> getAsteroids(){ 
-    ArrayList<NetworkActor> ret = new ArrayList<NetworkActor>();
-    NetworkActor[] actors = super.getState().getActors();
-    for(int i = actors.length -1; i>=0;i--){
-      NetworkActor n=actors[i];
-      if((n.getType().equals("asteroid"))){
-        ret.add(n);
-      }
+    private ArrayList<NetworkActor> asteroids;
+    //800 by 600 screen per person
+    public Sensors(Client client, GameState state){
+        super(client,state);
+        asteroids = getAsteroids(); 
     }
-    return ret;
-  }
-  public void act()
-  {
-    asteroids = getAsteroids();
-   super.act(); 
-  }
-  //public int Screen 
+
+    public ArrayList<NetworkActor> getAsteroids(){ 
+        ArrayList<NetworkActor> ret = new ArrayList<NetworkActor>();
+        NetworkActor[] actors = super.getState().getActors();
+        for(int i = actors.length -1; i>=0;i--){
+            NetworkActor n=actors[i];
+            if((n.getType().equals("asteroid"))){
+                ret.add(n);
+            }
+        }
+        return ret;
+    }
+
+    public void act()
+    {
+        asteroids = getAsteroids();
+        super.act(); 
+    }
+    //public int Screen 
 }
